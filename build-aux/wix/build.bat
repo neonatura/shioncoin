d21854de2372cc7d53d8448de54d1e19f8d042cc
Brian Burrell <bburrell.brian@gmail.com>
2019-08-13 19:43:59 -0600

    v4.1
    build/release file update

--- build-aux/wix/build.bat
+++ build-aux/wix/build.bat
@@ -1,14 +1,17 @@
 @rem Copyright 2017 Neo Natura
 @echo off
 
-set PKG=ShionCoin
-set VER=3.02
+set PKG=shioncoin
+set VER=4.1
 
+set SDK="C:\Program Files (x86)\Windows Kits\10\App Certification Kit\"
 set WIX="C:\program files (x86)\wix toolset v3.11\bin"
 set BUILD=C:\msys64\home\root\src\shioncoin\build
 set RELEASE=C:\release\shioncoin\bin
 
+copy /y %BUILD%\src\shioncoin\shcoind.exe > nul
 copy /y %BUILD%\src\shioncoin\.libs\shcoind.exe > nul
+copy /y %BUILD%\src\coin-console\shc.exe > nul
 copy /y %BUILD%\src\coin-console\.libs\shc.exe > nul
 move /y shcoind.exe %RELEASE% > nul
 move /y shc.exe %RELEASE% > nul
@@ -17,3 +20,5 @@ move /y shc.exe %RELEASE% > nul
 %WIX%\light -nologo -ext "c:\Program Files (x86)\WiX Toolset v3.11\bin\WixUtilExtension.dll" shioncoin.wixobj
 move /y shioncoin.msi %PKG%-%VER%.msi
 move /y %PKG%-%VER%.msi %RELEASE%
+
+rem %SDK%\signtool.exe sign /f "c:\certificates\neonatura.pfx" /p %1 /d "Neo Natura" /t http://timestamp.verisign.com/scripts/timstamp.dll /v %PKG%-%VER%.msi
